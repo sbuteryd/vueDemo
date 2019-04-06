63805b859288d6bb34dc16534ba6f8b68689590f
sbuteryd@qq.com <sbuteryd@qq.com>
2019-04-07 04:59:27 +0800

    4-1 列出渲染

--- js/main.js
+++ js/main.js
@@ -4,5 +4,16 @@ var app = new Vue({
         product:'socks',
         image:'./assets/vmSocks-green-onWhite.jpg',
         inStock:true,
+        details:["80% cotton","20% polyester","Gender-neutral"],
+        variants:[
+            {
+                variantId: 2234,
+                variantColor: "green"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue"
+            }
+        ]
     }
 })
