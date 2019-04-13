dad5b3ab050e97cfe026b08a5626da2278e23196
sbuteryd@qq.com <sbuteryd@qq.com>
2019-04-13 16:50:20 +0800

    lesson 5  事件处理

--- js/main.js
+++ js/main.js
@@ -2,18 +2,29 @@ var app = new Vue({
     el:'#app',
     data:{
         product:'Socks',
-        image:"./assets/vmSocks-green-onWhite.jpg",
+        image:'./assets/vmSocks-green-onWhite.jpg',
         inStock:true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                varianImage:'./assets/vmSocks-green-onWhite.jpg'
             },
             {
                 variantId: 2235,
-                variantColor: 'blue'
-            }
-        ]
+                variantColor: 'blue',
+                varianImage:'./assets/vmSocks-blue-onWhite.jpg'
+            },
+        ],
+        cart:0,
+    },
+    methods:{
+        addToCart(){
+            this.cart +=1
+        },
+        updateProduct(picture){
+            this.image = picture
+        }
     }
-})
+});
