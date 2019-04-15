d164bb524173019c1d1b77222fe637de2fdb9e79
sbuteryd@qq.com <sbuteryd@qq.com>
2019-04-15 09:42:07 +0800

    lesson 7 计算属性

--- js/main.js
+++ js/main.js
@@ -2,19 +2,21 @@ var app = new Vue({
     el:'#app',
     data:{
         product:'Socks',
-        image:'./assets/vmSocks-green-onWhite.jpg',
-        inStock:true,
+        brand:'Vue Mastery',
+        selectedVariant:0,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
-                varianImage:'./assets/vmSocks-green-onWhite.jpg'
+                varianImage:'./assets/vmSocks-green-onWhite.jpg',
+                variantQuantity:10,
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
-                varianImage:'./assets/vmSocks-blue-onWhite.jpg'
+                varianImage:'./assets/vmSocks-blue-onWhite.jpg',
+                variantQuantity:0
             },
         ],
         cart:0,
@@ -23,8 +25,20 @@ var app = new Vue({
         addToCart(){
             this.cart +=1
         },
-        updateProduct(picture){
-            this.image = picture
+        updateProduct(index){
+            this.selectedVariant = index
+        }
+    },
+    computed:{
+        title(){
+            return this.brand + '' + this.product
+        },
+        image(){
+            console.log(this.selectedVariant,'varianImage')
+            return this.variants[this.selectedVariant].varianImage
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
         }
     }
 });
